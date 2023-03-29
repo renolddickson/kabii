c69d2c85bcc94eab7d957fa2745207942595f070
smarthood <renolddickson18@gmail.com>
2023-03-29 18:32:41 +0530

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LobbyComponent } from './core/lobby/lobby.component';
+import { LobbyComponent } from './core/components/lobby/lobby.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { MainComponent } from './core/components/main/main.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/lobby', pathMatch: 'full' },
   { path: 'lobby', component: LobbyComponent },
+  {
+    path: 'nav',
+    component: NavbarComponent,
+    children: [{ path: 'main', component: MainComponent }],
+  },
 ];
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LobbyComponent } from './lobby/lobby.component';
-
-
+import { LobbyComponent } from './components/lobby/lobby.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
-  declarations: [
-    LobbyComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LobbyComponent, NavbarComponent, MainComponent],
+  imports: [CommonModule],
 })
-export class CoreModule { }
+export class CoreModule {}
