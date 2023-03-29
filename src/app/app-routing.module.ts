c56e3e067aaca93a2e5975c58226388934a3d4d1
smarthood <renolddickson18@gmail.com>
2023-03-29 17:47:39 +0530

    fdr

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LobbyComponent } from './core/lobby/lobby.component';
 
-const routes: Routes = [{ path: '', redirectTo: '/lobby', pathMatch: 'full' }];
+const routes: Routes = [
+  { path: '', redirectTo: '/lobby', pathMatch: 'full' },
+  { path: 'lobby', component: LobbyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
