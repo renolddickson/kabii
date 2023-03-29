32bca20a457b2f985de2f8b6b2b87029b35fa77d
smarthood <renolddickson18@gmail.com>
2023-03-29 18:39:00 +0530

    router-outet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CoreModule } from './core/core.module';
+import { RouterModule } from '@angular/router';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { LobbyComponent } from './components/lobby/lobby.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainComponent } from './components/main/main.component';
-
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [LobbyComponent, NavbarComponent, MainComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class CoreModule {}
