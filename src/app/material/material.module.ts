52fb8bfe79bc40bd08bb0ce03115198146b89c5a
ArockyaJ <arockyajoseph@gmail.com>
2023-03-30 09:49:10 +0530

    ghjv

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,118 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { MatRippleModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatButtonToggleModule,
+    MatSortModule,
+    MatCheckboxModule,
+    MatTableModule,
+    FlexLayoutModule,
+    RouterModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatTabsModule,
+    MatMenuModule,
+    MatRippleModule,
+    MatExpansionModule,
+    MatTooltipModule,
+    MatDatepickerModule,
+    MatSnackBarModule,
+    MatRadioModule,
+    MatSliderModule,
+    MatStepperModule,
+    MatChipsModule,
+    MatSlideToggleModule,
+    MatProgressBarModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatDividerModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatPaginatorModule,
+    MatButtonToggleModule,
+    MatSortModule,
+    MatCheckboxModule,
+    MatTableModule,
+    FlexLayoutModule,
+    RouterModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatTabsModule,
+    MatMenuModule,
+    MatRippleModule,
+    MatExpansionModule,
+    MatTooltipModule,
+    MatDatepickerModule,
+    MatSnackBarModule,
+    MatRadioModule,
+    MatSliderModule,
+    MatStepperModule,
+    MatChipsModule,
+    MatSlideToggleModule,
+    MatProgressBarModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatDividerModule,
+  ]
+})
+export class MaterialModule { }
