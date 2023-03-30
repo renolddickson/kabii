3bfa537d34143569fbba1401ac178dafa2775443
ArockyaJ <arockyajoseph@gmail.com>
2023-03-30 09:53:59 +0530

    mkk

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,8 +4,12 @@ import { LobbyComponent } from './components/lobby/lobby.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainComponent } from './components/main/main.component';
 import { RouterModule } from '@angular/router';
+
+
+
 @NgModule({
   declarations: [LobbyComponent, NavbarComponent, MainComponent],
   imports: [CommonModule, RouterModule],
 })
+
 export class CoreModule {}
