33f759587d307a049a7052567c4f27b47f19e60a
1SordFish1 <achuothan.marimuthu@gmail.com>
2023-03-30 18:59:44 +0530

    nav

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainComponent } from './components/main/main.component';
 import { MaterialModule } from '../material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { GroupChatComponent } from './components/group-chat/group-chat.component';
 
 @NgModule({
-  declarations: [LobbyComponent, NavbarComponent, MainComponent],
+  declarations: [LobbyComponent, NavbarComponent, MainComponent, GroupChatComponent],
   imports: [CommonModule, MaterialModule, ReactiveFormsModule],
 })
 export class CoreModule {}
