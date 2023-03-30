a8738af156f0cc0f50fa8fbff0f378b6d8acc4a0
smarthood <renolddickson18@gmail.com>
2023-03-30 09:48:19 +0530

    material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, RouterModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, RouterModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
